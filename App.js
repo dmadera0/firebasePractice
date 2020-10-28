c62740eef55cdb5d95ebf32e3eae822f10cfb179
Daniel <dmadera0@gmail.com>
2020-10-28 09:46:40 -0600

    first commit

--- App.js
+++ App.js
@@ -2,6 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDS71vPK6Rm6QFvxqS6hRu6xS4aZ2Bn48A",
+  authDomain: "react-app-mod5.firebaseapp.com",
+  databaseURL: "https://react-app-mod5.firebaseio.com",
+  projectId: "react-app-mod5",
+  storageBucket: "react-app-mod5.appspot.com",
+  messagingSenderId: "256285827591",
+  appId: "1:256285827591:web:8683f480d0d6ddb2e540a2",
+  measurementId: "G-6MYC3RMQPB"
+})
+
 export default function App() {
   return (
     <View style={styles.container}>
